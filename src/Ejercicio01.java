40fe52725106d1460f6052bbcb1f57861bff0727
Andre Aguirre <andreaguirre007@gmail.com>
2025-11-06 23:08:11 -0600

    coso de admin

--- src/Ejercicio01.java
+++ src/Ejercicio01.java
@@ -3,8 +3,16 @@
     Dada el arreglo de números, filtrar sólo los números pares.
  */
 
+import java.util.Arrays;
+
 public class Ejercicio01 {
     public static void main(String[] args) {
         int[] arreglo = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] pares = Arrays.stream(arreglo).filter(n -> n % 2 == 0).toArray();
+
+        for(int p : pares) {
+            System.out.println(p);
+        }
     }
 }
